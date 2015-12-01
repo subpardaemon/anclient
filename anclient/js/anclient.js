4255b9fc4e0e78ad0ca48c731325c659da1fca28
subpardaemon <pdx@pdx.hu>
2015-12-01 16:29:58 +0100

    added bootstrap navbar

--- anclient/js/anclient.js
+++ anclient/js/anclient.js
@@ -4,10 +4,16 @@
 
 var anclient = {
 		sessionid: null,
+		serverreply: null,
 		urlprefix: 'http://assist-network.herokuapp.com/',
 		taxonomy: {},
 		ajaxap: function(url,params) {
-			if (url=='') url = anclient.urlprefix+'api/com';
+			if (url=='') {
+				url = anclient.urlprefix+'api/com';
+			}
+			if (typeof params=='undefined') {
+				params = {};
+			}
 			params['sessionid'] = anclient.sessionid;
 			return new Promise(function(res,rej) {
 				$.ajax(url,{
@@ -15,33 +21,39 @@ var anclient = {
 						$('.loading').show();
 						return true;
 					},
-					complete:function() {
-						$('.loading').hide();
-					},
 					cache: false,
 					data:params,
 					dataType:'json',
 					error:function(xhr,tst,err) {
+						$('.loading').hide();
 						rej(tst,err);
 					},
 					method:'POST',
 					success:function(dt) {
+						$('.loading').hide();
 						res(dt);
 					}
 				});
 			});
 		},
 		init: function() {
-			anclient.ajaxap('js/taxonomy.json').then(function(d) {
+			anclient.ajaxap('http://www.pdx.hu/jobs/an/js/taxonomy.json').then(function(d) {
 				anclient.taxonomy = d;
 				console.log('ext: loaded taxonomy db');
 				anclient.initreal();
-			},function(x,stat,err) {
+			},function(stat,err) {
 				console.log('ext.err: '+stat+' '+err);
 			});
 		},
 		initreal: function() {
-			
+			$(".dropdown-toggle").dropdown();
+			/*
+			anclient.ajaxap(anclient.urlprefix+'api/').then(function(d) {
+				anclient.serverreply = d;
+			},function(stat,err) {
+				console.log('ext.err: '+stat+' '+err);
+			});
+			*/
 		}
 };
 
