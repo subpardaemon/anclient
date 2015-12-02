0ba64df4bc23c7437e5ed5cf270076885aafe3e0
subpardaemon <pdx@pdx.hu>
2015-12-03 00:20:01 +0100

    added GET and POST differentiated calls added geolocation *untested started work on form filler method

--- anclient/js/anclient.js
+++ anclient/js/anclient.js
@@ -8,7 +8,20 @@ var anclient = {
 		lang: 'hu',
 		urlprefix: 'http://assist-network.herokuapp.com/',
 		taxonomy: {},
-		ajaxap: function(url,params) {
+		currpos: '47.539063,19.049691',
+		ajaxpostap: function(url,params) {
+			if (typeof params=='undefined') {
+				params = {};
+			}
+			return anclient.ajaxap(url,params,'POST');
+		},
+		ajaxgetap: function(url,params) {
+			if (typeof params=='undefined') {
+				params = {};
+			}
+			return anclient.ajaxap(url,params,'GET');
+		},
+		ajaxap: function(url,params,meth) {
 			if (url=='') {
 				url = anclient.urlprefix+'api/com';
 			}
@@ -17,6 +30,9 @@ var anclient = {
 			}
 			if (typeof params=='undefined') {
 				params = {};
+			}
+			if (typeof meth=='undefined') {
+				meth = 'GET';
 			}
 			params['sessionid'] = anclient.sessionid;
 			return new Promise(function(res,rej) {
@@ -32,13 +48,15 @@ var anclient = {
 						$('.loading').hide();
 						rej(tst,err);
 					},
-					method:'GET',
+					method:meth,
 					success:function(dt) {
 						$('.loading').hide();
 						res(dt);
 					}
 				});
 			});
+		},
+		posn: function() {
 		},
 		init: function() {
 			if (localStorage.getItem('an-taxonomy')===null) {
@@ -57,11 +75,53 @@ var anclient = {
 		},
 		initreal: function() {
 			$(".dropdown-toggle").dropdown();
-			anclient.ajaxap('/api/').then(function(d) {
+			if (localStorage.getItem('an-lastlatlon')===null) {
+				anclient.currpos = '47.539063,19.049691';
+			} else {
+				anclient.currpos = localStorage.getItem('an-lastlatlon');
+			}
+			navigator.geolocation.getCurrentPosition(function(psn) {
+				anclient.currpos = psn.coords.latitude.toString()+psn.coords.longitude.toString();
+				localStorage.setItem('an-lastlatlon',anclient.currpos);
+			},function() {
+				console.log('geo.err: no geolocation available, using last known pos');
+			});
+			anclient.ajaxgetap('/api/').then(function(d) {
 				anclient.serverreply = d;
 			},function(stat,err) {
 				console.log('ext.err: '+stat+' '+err);
 			});
+		},
+		vals2form: function(formslc,fdata) {
+			var e,et,ha;
+			$(formslc+' [type="checkbox"]').prop('checked',false);
+			for(var n in fdata) {
+				ha = false;
+				e = $(formslc+' [name="'+n+'"]');
+				if (e.length>0) {
+					e = e.first();
+					et = e.attr('type');
+					if (et!==undefined) {
+						et = et.toLowerCase();
+						if (et=='radio') {
+							$(formslc+' [name="'+n+'"]').filter('[value="'+fdata[n].toString()+'"]').prop('checked',true);
+							ha = true;
+						}
+						else if (et=='checkbox') {
+							if (e.attr('value')==fdata[n]) {
+								e.prop('checked',true);
+								ha = true;
+							}
+						}
+					}
+					if (ha===false) {
+						
+					}
+				} else {
+					console.log('form.err: no matching field for '+n+' under selector '+formslc);
+				}
+			}
+			
 		}
 };
 
